8f13cd103af6a755835cd8d9c0338001f5afedba
dobernike <Dobernike@mail.ru>
2018-02-12 16:11:08 +0300

    init

--- basket.js
+++ basket.js
@@ -4,114 +4,115 @@ let pricingEa = elId[1].getAttribute('selected');
 ///////////////////////////////////// Корзина: Основной склад ///////////////////////////////////////////
 let priceEa = document.querySelectorAll('.eaOptList tbody tr');
 // Проверка, что режим включен "EA розница" 
-if (pricingEa == 'selected' && priceEa.length > 0) 
-{
-    
+if (pricingEa == 'selected' && priceEa.length > 0) {
+
     console.log('I`m here');
     let priceEa = document.querySelectorAll('.eaOptList tbody tr');
     let x = 0;
     let price;
     let realSumm = document.getElementsByTagName('h3')[0].innerText.match(/Итого: \b(\d*)/)[1];
-    switch(priceEa.length) { //костыль
+    switch (priceEa.length) { //костыль
         case 1:
         case 2:
         case 3:
         case 4:
         case 5:
-        timeout = 1200;
-        break;
+            timeout = 1500;
+            break;
         case 6:
         case 7:
         case 8:
         case 9:
         case 10:
-        timeout = 1700;
-        break;
+            timeout = 1700;
+            break;
         default:
-        timeout = 2700;
+            timeout = 2700;
     }
 
-    for (let i = 0; i < priceEa.length; i++) 
-    {
+    for (let i = 0; i < priceEa.length; i++) {
         let firm = priceEa[i].cells[2].innerText;
         let art = priceEa[i].cells[3].innerText;
         let itemName = priceEa[i].cells[4].innerText;
         let cost = priceEa[i].cells[5].innerText;
-        let url = "https://euroauto.ru/searchnr/"+art;
+        let url = "https://euroauto.ru/searchnr/" + art;
         let url2 = "https://euroauto.ru/firms/" + firm + "/" + art + "/";
 
         // cross xhr для выявления розничной цены
         fetch(url)
-  .then((resp) => resp.text())
-  .then(function(data) {
-    console.log('request succeeded with JSON response')
-    let price = data.match(
-        /(<span .* itemprop="price".*>|<span .*price_num_real.*>)(.*?)<\/span>/
-      );
-
-      if (price) {
-        price = price[2].replace(" ", "");
-
-        if (itemName.toLowerCase().indexOf("масло") > -1) {
-             price = roundDown((price), 100);
-        } else {
-            // Устанавливается скидка 15%
-             price = roundUp((price) * 0.85, 50); 
-        }
-        // Заменяется цена и сумма на скидочную
-         priceEa[i].cells[5].innerText = price;
-         priceEa[i].cells[7].innerText = (price * priceEa[i].cells[6].innerText);
-         // Высчитывается общая сумма
-        x += Number(priceEa[i].cells[7].innerText);
-           
-        } else {
-            console.log('search with firm');
-            fetch(url2)
             .then((resp) => resp.text())
-            .then(function(data) {
-                price = data.match(/(<span .* itemprop="price".*>|<span .*price_num_real.*>)(.*?)<\/span>/);
-            
-                          if (price) {
-                            price = price[2].replace(" ", "");
-            
-                            if (itemName.toLowerCase().indexOf("масло") > -1) {
-                                 price = roundDown((price), 100);
+            .then(function (data) {
+                console.log('request succeeded with JSON response')
+                let price = data.match(
+                    /(<span .* itemprop="price".*>|<span .*price_num_real.*>)(.*?)<\/span>/
+                );
+
+                if (price) {
+                    price = price[2].replace(" ", "");
+
+                    if (itemName.toLowerCase().indexOf("масло") > -1) {
+                        price = roundDown((price), 100);
+                    } else {
+                        // Устанавливается скидка 15%
+                        price = roundUp((price) * 0.85, 50);
+                    }
+                    // Заменяется цена и сумма на скидочную
+                    priceEa[i].cells[5].innerText = price;
+                    priceEa[i].cells[7].innerText = (price * priceEa[i].cells[6].innerText);
+                    // Высчитывается общая сумма
+                    x += Number(priceEa[i].cells[7].innerText);
+
+                } else {
+                    console.log('search with firm');
+                    fetch(url2)
+                        .then((resp) => resp.text())
+                        .then(function (data) {
+                            price = data.match(/(<span .* itemprop="price".*>|<span .*price_num_real.*>)(.*?)<\/span>/);
+
+                            if (price) {
+                                price = price[2].replace(" ", "");
+
+                                if (itemName.toLowerCase().indexOf("масло") > -1) {
+                                    price = roundDown((price), 100);
+                                } else {
+                                    // Устанавливается скидка 15%
+                                    price = roundUp((price) * 0.85, 50);
+                                }
+                                // Заменяется цена и сумма на скидочную
+                                priceEa[i].cells[5].innerText = price;
+                                priceEa[i].cells[7].innerText = (price * priceEa[i].cells[6].innerText);
+                                // Высчитывается общая сумма
+                                x += Number(priceEa[i].cells[7].innerText);
+
                             } else {
-                                // Устанавливается скидка 15%
-                                 price = roundUp((price) * 0.85, 50); 
+                                console.log('error');
                             }
-                            // Заменяется цена и сумма на скидочную
-                             priceEa[i].cells[5].innerText = price;
-                             priceEa[i].cells[7].innerText = (price * priceEa[i].cells[6].innerText);
-                             // Высчитывается общая сумма
-                            x += Number(priceEa[i].cells[7].innerText);
+                        })
 
-                } else {
-                    console.log('error');
-                }})
-                
-  }}).catch(function (error) {
-    console.log('request failed', error)
-  })
-
-           
-
-        
-
-            if (i == (priceEa.length - 1)) {                                                    
-                setTimeout(function(){                                                         
-                    if (x < 3000){                                                         
-                    document.getElementsByTagName('h3')[0].innerText = "Итого с доставкой: "+(x+300)+" руб. "+" Прибыль: " + roundUp((((x+300)-realSumm)/2), 10);
-                } else {
-                    document.getElementsByTagName('h3')[0].innerText = "Итого: "+x+" руб. "+ " Прибыль: " + roundUp(((x-realSumm)/2), 10);
                 }
-                }, timeout); //костыль                                                                   
-             }  }        
-                                                                                     
-             console.log('end');
- 
-        
-            } else {
+            }).catch(function (error) {
+                console.log('request failed', error)
+            })
+
+
+
+
+
+        if (i == (priceEa.length - 1)) {
+            setTimeout(function () {
+                if (x < 3000) {
+                    document.getElementsByTagName('h3')[0].innerText = "Итого с доставкой: " + (x + 300) + " руб. " + " Прибыль: " + roundUp((((x + 300) - realSumm) / 2), 10);
+                } else {
+                    document.getElementsByTagName('h3')[0].innerText = "Итого: " + x + " руб. " + " Прибыль: " + roundUp(((x - realSumm) / 2), 10);
+                }
+            }, timeout); //костыль                                                                   
+        }
+    }
+
+    console.log('end');
+
+
+} else {
     alert('Выбери "ЕА розница" и добавьте товары в корзину!');
 }
 
@@ -119,10 +120,10 @@ if (pricingEa == 'selected' && priceEa.length > 0)
 //////////////////////////////////////////////functions///////////////////////////////////////////////////////
 
 function getAjax(url, success) {
-   let xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('GET', url);                                                   //ассинхронность?
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+    let xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('GET', url); //ассинхронность?
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
@@ -137,7 +138,6 @@ function roundUp(num, precision) {
     return Math.round(num / precision) * precision;
 }
 
-  function roundDown(num, precision) {
+function roundDown(num, precision) {
     return Math.floor(num / precision) * precision;
 }
-
