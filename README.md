733ec679695bff148fa486a6117305b5f04268b9
dobernike <dobernike@mail.ru>
2018-04-07 15:56:07 +0300

    ctrl s

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # EAextended
+
 This extenshen for google chrome and firefox.
 In future Opera, Yandex browser, EDGE and last IE, Electron and React.Native
--- marketplace.js
+++ marketplace.js
@@ -1,23 +1,23 @@
-// Под комментами селекторы jquery, а за ними селекторы es6(js); 
+// Под комментами селекторы jquery, а за ними селекторы es6(js);
 // let priceEa = $('#pr .pricing-ea');
-let priceEa = document.querySelectorAll('#pr .pricing-ea');
+let priceEa = document.querySelectorAll("#pr .pricing-ea");
 // let elId = $('#eaopt_opts_pricing');
-let elId = document.getElementById('eaopt_opts_pricing');
+let elId = document.getElementById("eaopt_opts_pricing");
 // let pricingEa = $(elId[1]).attr('selected');
-let pricingEa = elId[1].getAttribute('selected');
+let pricingEa = elId[1].getAttribute("selected");
 
-if (pricingEa == 'selected') {
+if (pricingEa == "selected") {
   for (let i = 0; i < priceEa.length; i++) {
     let b = priceEa[i].innerText;
-    b = b.replace(' ', '')
+    b = b.replace(" ", "");
     let c = b * 0.85;
     let d = roundR(c, 50);
     priceEa[i].innerText = d;
-  };
+  }
 } else {
   alert('Выбери "ЕА розница"!');
-};
+}
 
 function roundR(num, precision) {
   return Math.round(num / precision) * precision;
-};
+}
