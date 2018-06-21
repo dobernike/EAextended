aa59d0cf133553463c0a6b8fd919a71e9bdbf4e5
dobernike <Dobernike@mail.ru>
2018-06-21 12:54:19 +0300

    init

--- historyOpt.js
+++ historyOpt.js
@@ -1,9 +1,21 @@
 console.clear('console is cleared');
 
+const seller = document.getElementsByClassName('dropdown-toggle')[1].innerText; // = "Продавец "
+
+if (seller == "Продавец ") {
+
+    console.log('good');
+
 const total = document.getElementsByClassName(
-'rubl')[0];
-const seller = document.getElementsByClassName('dropdown-toggle')[0];//.innerText = "Продавец " 
+    'rubl')[0];
+const commentIcon = document.getElementsByClassName('history-action-link order-comment')[0]; //.after text прибыль
+let goods = document.getElementsByClassName('history-descr-content');
+// goods[i].getEmlementsByTag('a') href
 let price = document.getElementsByClassName("history-price-num");
-const commentIcon = document.getElementsByClassName('history-action-link order-comment')[0];//.after text прибыль
+let quantity = document.getElementsByClassName('history-quantity');
 
 //Нужно делать через серч поиск цены розница
+
+} else {
+    console.log('bad, seller is: ' + seller);
+}
