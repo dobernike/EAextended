2915b5d471ab7e4f3d1f50276b54eecf371c4538
dobernike <Dobernike@mail.ru>
2018-08-13 18:51:51 +0300

    done basket

--- basket.js
+++ basket.js
@@ -7,92 +7,47 @@ if (profile == "Продавец ") {
     /**
      *  Корзина: Основной склад
      **/
-
     let priceEa = document.getElementsByClassName("cart-store-brand");
-    // console.log(priceEa);
     let priceOpt = document.getElementsByClassName("cart-store-price-value");
     let summ = 0;
     let cnt = 0;
 
-    // let x = 0;
-    // let price;
-    // let realSumm = document
-    //   .getElementsByTagName("h3")[0]
-    //    .innerText.match(/Итого: \b(\d*)/)[1];
-    //   switch (priceEa.length) {
-    //     case 1:
-    //     case 2:
-    //     case 3:
-    //     case 4:
-    //     case 5:
-    //       timeout = 4000;
-    //       break;
-    //     case 6:
-    //     case 7:
-    //     case 8:
-    //     case 9:
-    //     case 10:
-    //       timeout = 4500;
-    //       break;
-    //     default:
-    //       timeout = 5000;
-    //   }
-    // var itemName = '';
-
     for (let i = 0; i < priceEa.length; i++) {
-      // console.log(priceEa[i]);
       let firm = priceEa[i].innerText;
       let art = priceEa[i].previousElementSibling.innerText;
       let itemName = priceEa[i].nextElementSibling;
       let itemProp = itemName.innerText;
-      // Временный костыль для розницы
+      //Наименование для розницы
       if (itemName.innerText == "Новый") {
         itemName = priceEa[i].nextElementSibling.nextElementSibling;
       }
       let cost = priceOpt[i].innerText;
-      // console.log(firm);
-      // console.log(art);
-      // console.log(itemName);
-      // console.log(cost);
       let itemNameOil = itemName.innerText;
       itemNameOil = itemNameOil.match(/\Масло/);
-      //  console.log(itemName);
 
       let url = "https://euroauto.ru/searchnr/" + art;
-      //    console.log(url);
       //     let url2 = "https://euroauto.ru/firms/" + firm + "/" + art + "/";
 
       // cross xhr для выявления розничной цены
       fetch(url)
         .then(resp => resp.text())
         .then(function (data) {
-          // console.log("request succeeded with JSON response");
-          //  console.log(itemName);
           let price = data.match(
             /<span\W*itemprop="price"\W*content=".*">(\d.*)<\/span><span itemprop="priceCurrency" content="RUB">руб.<\/span>/m
-            // /<div.class="num-from-block main-store">.*\W.*<div.class="num-price">(\d.*)/
           );
-          //console.log(data);
-          //console.log(price);
 
           if (!price) {
             price = data.match(
               /<div.class="text-left btn btn-default active">\W.*\W.*\W.*\W.*<span.class="price">(\d.*)<\/span>.*<\/span>/
             );
           }
-          //console.log('cost is: ' + cost);
           price = price[1].replace(" ", "");
-          // console.log('price is: ' + price);
           var newPrice = 0;
-          //  console.log(itemName.previousSibling.innerText);
           if (itemNameOil == "Масло") {
-            //console.log("good");
             newPrice = roundDown(price, 50);
           } else if (itemProp == "Новый") {
-            // console.log('good');
             newPrice = price;
           } else {
-            // console.log("bad");
             // Устанавливается скидка 15%
             var newCurentPrice = round(price * 0.85, 50);
             var newFixedPrice = roundDown(price, 50);
@@ -104,7 +59,6 @@ if (profile == "Продавец ") {
           }
           priceOpt[i].setAttribute('style', 'color: #337ab7;');
           priceOpt[i].innerText = newPrice;
-          //  console.log('new is: ' + newPrice);
         })
         .then(function () {
           cnt += 1;
@@ -124,13 +78,10 @@ if (profile == "Продавец ") {
               ) {
 
                 let cartStoreWrapOptSum = 0;
-                // console.log("good");
-                //console.log(cartStoreWrap);
                 let cartStoreWrapItem = cartStoreWrap.getElementsByClassName(
                   "cart-store-item-wrap"
                 );
                 let cartStoreWrapOptOriginalSum = cartStoreWrapItem[cartStoreWrapItem.length - 1].getElementsByClassName('cart-store-total-sum-value')[0].innerText;
-                // console.log(cartStoreWrapOptOriginalSum);
 
                 for (let k = 0; k < cartStoreWrapItem.length - 1; k++) {
 
@@ -138,7 +89,6 @@ if (profile == "Продавец ") {
                       k
                     ].getElementsByClassName("cart-store-price-value")[0]
                     .innerText;
-                  // console.log(cartStoreWrapItemPrice);
                   cartStoreWrapOptSum += Number(cartStoreWrapItemPrice);
 
                 }
@@ -151,39 +101,44 @@ if (profile == "Продавец ") {
                 }
 
                 let profitOpt = Number(cartStoreWrapOptSum) - Number(cartStoreWrapOptOriginalSum);
-                // console.log(cartStoreWrapOptSum);
-
-                // console.log('profit: ' + profitOpt);
                 let profitOptCeil = roundUp(profitOpt / 2, 50);
                 cartStoreWrapItem[cartStoreWrapItem.length - 1].getElementsByClassName('cart-store-total-sum-value')[0].innerText = cartStoreWrapOptSum + ' руб ' + deliveryOpt + ' | Прибыль: ' + profitOptCeil;
               } else {
                 // Остальные склады
+                let cartStoreWrapEaSum = 0;
+                let cartStoreWrapItemEa = cartStoreWrap.getElementsByClassName(
+                  "cart-store-item-wrap"
+                );
+                let cartStoreWrapEaOriginalSum = cartStoreWrapItemEa[cartStoreWrapItemEa.length - 1].getElementsByClassName('cart-store-total-sum-value')[0].innerText;
+
+                cartStoreWrapItemEa[cartStoreWrapItemEa.length - 1].getElementsByClassName('cart-store-num-comment')[0].innerText = '';
+
+                for (let l = 0; l < cartStoreWrapItemEa.length - 1; l++) {
+
+                  let cartStoreWrapItemEaPrice = cartStoreWrapItemEa[
+                      l
+                    ].getElementsByClassName("cart-store-price-value")[0]
+                    .innerText;
+                  cartStoreWrapEaSum += Number(cartStoreWrapItemEaPrice);
+
+                }
+                let profitEa = Number(cartStoreWrapEaSum) - Number(cartStoreWrapEaOriginalSum);
+                let deliveryEa = '';
+
+                if (cartStoreWrapEaSum < 3000) {
+                  deliveryEa = ' c доставой';
+                  cartStoreWrapEaSum += 300;
+                }
+                let profitEaCeil = 0;
+                if (profitEa >= 800) {
+                  profitEaCeil = roundUp(profitEa / 2, 50);
+                } else {
+                  profitEaCeil = profitEa;
+                }
+                cartStoreWrapItemEa[cartStoreWrapItemEa.length - 1].getElementsByClassName('cart-store-total-sum-value')[0].innerText = cartStoreWrapEaSum + ' руб ' + deliveryEa + ' | Прибыль: ' + profitEaCeil;
+
               }
             }
-            // let total = document.getElementsByClassName(
-            //   "cart-store-total-sum-value"
-            // );
-            // let totalsum = 0;
-            // let deliveryPrice = document.getElementsByClassName(
-            //   "delivery-price"
-            // );
-            // for (let j = 0; j < total.length; j++) {
-            //   totalsum += Number(total[j].innerText);
-
-            //   totalsum += Number(deliveryPrice[j].innerText);
-            // }
-            // let sklad = "";
-            // if (total.length > 1) {
-            //   sklad = " | " + total.length + " точки";
-            // }
-            // let totalprofits = Number(summ) - Number(totalsum);
-            // let totalprofit = roundUp(totalprofits / 2, 50);
-            // console.log("totalprofits: " + totalprofits);
-            // console.log("totalprofit: " + totalprofit);
-            // total[0].innerText = summ + sklad + " | Прибыль: " + totalprofit;
-            // console.log(summ);
-            // console.log(totalsum);
-            // console.log(summ - totalsum);
           }
         })
         .catch(function (error) {
@@ -272,10 +227,6 @@ if (profile == "Продавец ") {
 /**
  *  Функции
  **/
-// function toNumber(string) {
-//   return parseFloat(string.replace(",", "."));
-// }
-
 function round(num, precision) {
   return Math.round(num / precision) * precision;
 }
